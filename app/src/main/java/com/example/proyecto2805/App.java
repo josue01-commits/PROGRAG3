12a33ab89a740a0d7aaef5c7a2d7c7affaeba9ba
damaris <anabel.lloacana8415@utc.edu.ec>
2026-06-04 21:12:02 -0500

    variable de ventas grandes

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -11,6 +11,7 @@ public class App {
             final int LIMITE_MAXIMO = 5;
             double ventaMayor = 0;
             double ventaMenor = Double.MAX_VALUE;
+            int ventasGrandes = 0;
 
             System.out.println("--- Registro de Ventas Diarias(Maximo " + LIMITE_MAXIMO + " ventas)---");
             System.out.println("Ingrese las ventas una por una. Escriba 0 para finalizar el dia.\n");
