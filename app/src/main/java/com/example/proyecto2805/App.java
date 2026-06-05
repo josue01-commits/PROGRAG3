06aa949b9594257f3548383e7b31c3a8df5d0c98
josue <diegoreinoso2000>
2026-06-04 22:53:26 -0500

    16 random

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,12 +2,8 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int n = 10;
-        int suma = 0;
-        for (int i = 1; i <= n; i++) {
-            suma += i;
-        }
-        System.out.println("La suma del 1 al " + n + " es: " + suma);
+        int random = (int)(Math.random() * 100) + 1;
+        System.out.println("Número aleatorio: " + random);
     }
 }
   
