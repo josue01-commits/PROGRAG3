94931906ce12f9a3aa71bdf8ebebc1e90715b90b
josue <diegoreinoso2000>
2026-06-04 23:00:36 -0500

    24 Inversa

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,8 +2,12 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        String texto = "Desarrollo de Software";
-        System.out.println("La cadena tiene " + texto.length() + " caracteres.");
+        String original = "Hola";
+        String invertida = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            invertida += original.charAt(i);
+        }
+        System.out.println("Original: " + original + " | Invertida: " + invertida);
     }
 }
   
