928819be6f06270b1797fd52f3a619b473a371db
josue <diegoreinoso2000>
2026-06-04 22:45:23 -0500

    8 impar o par

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,11 +2,12 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        double base = 5.0;
-        double altura = 8.0;
-        double area = (base * altura) / 2;
-        System.out.println("El área del triángulo es: " + area);
+        int numero = 7;
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es par.");
+        } else {
+            System.out.println(numero + " es impar.");
     }
 }
-  
+  }
     
