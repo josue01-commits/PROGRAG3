5ad992f35be9553a47ff893f9c17836c8968e4be
josue <diegoreinoso2000>
2026-06-04 22:59:50 -0500

    23 longitud de cadena

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,14 +2,8 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int[] numeros = {5, 12, 3, 21, 8};
-        int mayor = numeros[0];
-        for (int num : numeros) {
-            if (num > mayor) {
-                mayor = num;
-            }
-        }
-        System.out.println("El número mayor es: " + mayor);
+        String texto = "Desarrollo de Software";
+        System.out.println("La cadena tiene " + texto.length() + " caracteres.");
     }
 }
   
