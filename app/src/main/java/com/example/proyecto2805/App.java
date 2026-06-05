ab8305da96f483a0895e294fa047c28e7ffee0d1
josue <diegoreinoso2000>
2026-06-04 22:49:08 -0500

    bucle for

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,10 +2,9 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        double largo = 10.0;
-        double ancho = 5.0;
-        double perimetro = 2 * (largo + ancho);
-        System.out.println("El perímetro es: " + perimetro);
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
   
