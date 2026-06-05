be14841c857102c4df5d0f1bd8db281fec0ac4b4
josue <diegoreinoso2000>
2026-06-04 22:52:26 -0500

    15 suma n

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,10 +2,12 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int numero = 5;
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(numero + " x " + i + " = " + (numero * i));
+        int n = 10;
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            suma += i;
         }
+        System.out.println("La suma del 1 al " + n + " es: " + suma);
     }
 }
   
