6675c4f44e39a8b5671d0c918c4361e555a24807
josue <diegoreinoso2000>
2026-06-04 22:47:22 -0500

    10 convertir

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,13 +2,9 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int num1 = 15;
-        int num2 = 20;
-        if (num1 > num2) {
-            System.out.println("El mayor es: " + num1);
-        } else {
-            System.out.println("El mayor es: " + num2);
-        }
+        double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + "°C equivalen a " + fahrenheit + "°F");
     }
 }
   
