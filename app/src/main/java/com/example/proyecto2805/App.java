6dd2fdf3cd656f87f3b126621e5fa088fa299960
josue <diegoreinoso2000>
2026-06-04 22:58:11 -0500

    21 promedio

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,9 +2,13 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i += 2) {
-            System.out.print(i + " ");
+        int[] numeros = {10, 20, 30, 40, 50};
+        double suma = 0;
+        for (int num : numeros) {
+            suma += num;
         }
+        double promedio = suma / numeros.length;
+        System.out.println("El promedio es: " + promedio);
     }
 }
   
