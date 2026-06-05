66516315631f0654ed4d5e67c1463ccc77afeb90
josue <diegoreinoso2000>
2026-06-04 22:57:17 -0500

    20 impar

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,12 +2,8 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int year = 2024;
-        boolean esBisiesto = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        if (esBisiesto) {
-            System.out.println(year + " es un año bisiesto.");
-        } else {
-            System.out.println(year + " no es bisiesto.");
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
         }
     }
 }
