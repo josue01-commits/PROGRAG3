3e6600ae4991740088692f390ca44952491af91e
josue <diegoreinoso2000>
2026-06-04 22:42:23 -0500

    6 suma

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,9 +2,11 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
+        int a = 5;
+        int b = 10;
+        int suma = a + b;
+        System.out.println("La suma es: " + suma);
     }
-    }
-
+}
   
     
