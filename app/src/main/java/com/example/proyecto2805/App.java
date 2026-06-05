2a005b22d66b9bdcb61b0d51b1c862b55f210786
josue <diegoreinoso2000>
2026-06-04 22:46:22 -0500

    9 mayor

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,12 +2,14 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int numero = 7;
-        if (numero % 2 == 0) {
-            System.out.println(numero + " es par.");
+        int num1 = 15;
+        int num2 = 20;
+        if (num1 > num2) {
+            System.out.println("El mayor es: " + num1);
         } else {
-            System.out.println(numero + " es impar.");
+            System.out.println("El mayor es: " + num2);
+        }
     }
 }
-  }
+  
     
