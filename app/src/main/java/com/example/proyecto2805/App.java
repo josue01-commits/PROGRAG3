225181e048083283a8950d68a49e67854515e63a
josue <diegoreinoso2000>
2026-06-04 22:43:56 -0500

    7 area

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,10 +2,10 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int suma = a + b;
-        System.out.println("La suma es: " + suma);
+        double base = 5.0;
+        double altura = 8.0;
+        double area = (base * altura) / 2;
+        System.out.println("El área del triángulo es: " + area);
     }
 }
   
