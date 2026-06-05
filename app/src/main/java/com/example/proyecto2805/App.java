9c2a90d422080d03d5931efcff35ff5eab9b4e9b
josue <diegoreinoso2000>
2026-06-04 22:54:16 -0500

    17 potencia

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,8 +2,10 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        int random = (int)(Math.random() * 100) + 1;
-        System.out.println("Número aleatorio: " + random);
+        double base = 3;
+        double exponente = 4;
+        double resultado = Math.pow(base, exponente);
+        System.out.println(base + " elevado a " + exponente + " es: " + resultado);
     }
 }
   
