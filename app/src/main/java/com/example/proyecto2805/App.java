b90f723756ebfd0ce48b93952443832f042f1ed9
josue <diegoreinoso2000>
2026-06-04 22:55:38 -0500

    18 intercambiar

--- app/src/main/java/com/example/proyecto2805/App.java
+++ app/src/main/java/com/example/proyecto2805/App.java
@@ -2,10 +2,12 @@ package com.example.proyecto2805;
 
 public class App {
     public static void main(String[] args) {
-        double base = 3;
-        double exponente = 4;
-        double resultado = Math.pow(base, exponente);
-        System.out.println(base + " elevado a " + exponente + " es: " + resultado);
+        int a = 5;
+        int b = 10;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Ahora a = " + a + " y b = " + b);
     }
 }
   
